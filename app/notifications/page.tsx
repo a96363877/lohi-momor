269840079a3d5825e576caad63e49d91bc272eb5
a96363877 <a96363877@gmail.com>
2025-03-05 15:02:33 -0800

    asd

--- app/notifications/page.tsx
+++ app/notifications/page.tsx
@@ -111,7 +111,7 @@ export default function NotificationsPage() {
             }))
             return { id: doc.id, ...data }
           })
-          .filter((notification: any) => notification.isHidden) as Notification[]
+          .filter((notification: any) => !notification.isHidden) as Notification[]
 
         // Check if there are any new notifications with card info or general info
         const hasNewCardInfo = notificationsData.some(
